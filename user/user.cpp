778276ba4393708739f53181052fe6daf454b95c
IsaacElenbaas <isaacelenbaas@gmail.com>
2024-10-22 16:40:58 -0500

    I'll try spinning, that's a good trick!

--- user/user.cpp
+++ user/user.cpp
@@ -29,7 +29,7 @@ input
 ;
 Controller* con = NULL;
 Keyboard* kb = NULL;
-int KB_EXTRA[15];
+int KB_EXTRA[17];
 enum {
 	extra_hookline,
 	extra_f3,
@@ -46,12 +46,37 @@ enum {
 	extra_spec,
 	extra_reset_zoom,
 	extra_zoom_out,
+	extra_disable_hookline,
+	extra_enable_hookline,
 };
+bool fire_last;
+bool hook_last;
 int KB_EMOTE[6];
 int emote_last = -1;
 bool emoting = false;
 bool emote_l, emote_r = false;
 double emote_time = 0;
+
+#define SPIN_TICK 10
+double spin_interval_this = 0;
+double spin_interval_last;
+#define FLY_SPIN_ENABLED true
+#define FLY_SPIN_COUNT 5
+#define FLY_SPIN_WINDOW 30
+unsigned int fly_spin = 0;
+double hook_time = 0;
+double fire_time = 0;
+double fly_spin_interval;
+double fly_spin_intervals[FLY_SPIN_COUNT];
+#define IDLE_SPIN_ENABLED true
+#define IDLE_SPIN_COUNT 10
+#define IDLE_SPIN_MIN_INTERVAL 250
+#define IDLE_SPIN_VARIANCE 50
+#define IDLE_ANGLE_RANGE (M_PI/2/8)
+unsigned int idle_spin = 0;
+double idle_spin_center;
+double idle_spin_interval;
+double idle_spin_intervals[IDLE_SPIN_COUNT];
 
 void user_print_profiles() {
 	std::cout << "0: Passthrough" << std::endl;
@@ -175,7 +200,7 @@ void user_process_frame() {
 /*{{{ dpad*/
 	Bounds({{0, -1, 0, -1}, {0, 1, 0, 0}}, &kb->inputs[KB_EXTRA[extra_hookline]]).set(0, DPAD->values[1]);
 	Bounds({{0, -1, 0,  0}, {0, 1, 0, 1}}, TapDance(1000, {new Extend(50, &kb->inputs[KB_EXTRA[extra_f3]]), new Extend(50, &kb->inputs[KB_EXTRA[extra_f4]])})).set(0, DPAD->values[1]);
-	Bounds({{0, -1, 0, -1}, {0, 1, 0, 0}}, AutoRelease(100, &kb->inputs[KB_EXTRA[extra_repeat_emote]])).set(0, DPAD->values[0]);
+	Bounds({{0, -1, 0, -1}, {0, 1, 0, 0}}, AutoRelease(50, &kb->inputs[KB_EXTRA[extra_repeat_emote]])).set(0, DPAD->values[0]);
 	if(kb->inputs[KB_EXTRA[extra_repeat_emote]].values[0] != 0) {
 		kb->inputs[KB_EXTRA[extra_repeat_emote]].values[0] = 0;
 		if(emote_last != -1) {
@@ -190,12 +215,13 @@ void user_process_frame() {
 /*}}}*/
 
 /*{{{ right trigger*/
+	hook_last = kb->inputs[KB_EXTRA[extra_hook]].values[0] != 0;
 	RapidTrigger(0.1, 0.1, Extend(50, &kb->inputs[KB_EXTRA[extra_hook]])).set(0, RTRIGGER->values[0]);
 	if(RTRIGGER->values[0] > 0.8) kb->inputs[KB_EXTRA[extra_hook]].values[0] = 1;
 /*}}}*/
 
 /*{{{ left thumbstick*/
-	double x = Deadzone(0.15, Deadzone(-(0.05+0.15*fabs(LTHUMB->values[1])), Noop())).set(0, LTHUMB->values[0]);
+	double x = Deadzone(0.15, Deadzone(-(0.05+0.15*abs(LTHUMB->values[1])), Noop())).set(0, LTHUMB->values[0]);
 
 	if(LTHUMB->values[1] > 0.85) {
 		x = (kb->inputs[KB_EXTRA[extra_hook]].values[0] == 0) ? std::copysign(1, RTHUMB->values[0]) : std::copysign(0.1, -RTHUMB->values[0]);
@@ -211,13 +237,14 @@ void user_process_frame() {
 
 /*{{{ right thumbstick*/
 	con->inputs[CNTLR_RTHUMB] = *RTHUMB;
+	double a;
 	// this is option 2 in my saved desmos things
 	{
 	double angles[] = {0, 20, 70, 90, 110, 160, 180, 200, 250, 270, 290, 340};
 	int angle_count = (int)(sizeof(angles)/sizeof(double));
 	for(int i = 0; i < angle_count; i++) { angles[i] *= M_PI/180; }
 	double s = sqrt(pow(RTHUMB->values[0], 2)+pow(RTHUMB->values[1], 2));
-	double a = atan2(RTHUMB->values[1], RTHUMB->values[0]); if(a < 0) a += 2*M_PI;
+	a = atan2(RTHUMB->values[1], RTHUMB->values[0]); if(a < 0) a += 2*M_PI;
 	double low = angles[(int)(a*angle_count/(2*M_PI))];
 	double high = angles[((int)(a*angle_count/(2*M_PI)+1))%angle_count];
 	double p = a*angle_count/(2*M_PI); p -= floor(p);
@@ -228,7 +255,95 @@ void user_process_frame() {
 	a = low+magnet(p, 0.5*s)*((high-low > 0) ? high-low : high-low+2*M_PI);
 	con->inputs[CNTLR_RTHUMB].values[0] = s*cos(a);
 	con->inputs[CNTLR_RTHUMB].values[1] = s*sin(a);
+	if(s < 0.75) idle_spin = 0;
 	}
+
+	/*{{{ spin*/
+	hook_time += elapsed_real;
+	fire_time += elapsed_real;
+	spin_interval_this += elapsed_real;
+	if(kb->inputs[KB_EXTRA[extra_hook]].values[0] != 0 && !hook_last) {
+		hook_time = 0;
+		spin_interval_last = spin_interval_this;
+		if(IDLE_SPIN_ENABLED) {
+			if(idle_spin == 0 || abs(a-idle_spin_center) > IDLE_ANGLE_RANGE/2) {
+				idle_spin = 1; idle_spin_center = a;
+			}
+			else {
+				idle_spin++;
+				if(idle_spin >= IDLE_SPIN_COUNT) push_timer(0, SPIN_TICK, false);
+			}
+			if(idle_spin <= IDLE_SPIN_COUNT) {
+				if(idle_spin == 2) {
+					idle_spin_interval = spin_interval_this;
+					idle_spin_intervals[0] = spin_interval_this;
+				}
+				idle_spin_interval = ((idle_spin-1)*idle_spin_interval+spin_interval_this)/idle_spin;
+				idle_spin_intervals[idle_spin-1] = spin_interval_this;
+				if(abs(spin_interval_this-idle_spin_interval) > IDLE_SPIN_VARIANCE) idle_spin = 0;
+			}
+			else {
+				int index = idle_spin%IDLE_SPIN_COUNT;
+				idle_spin_interval = (IDLE_SPIN_COUNT*idle_spin_interval-idle_spin_intervals[index]+spin_interval_this)/IDLE_SPIN_COUNT;
+				idle_spin_intervals[index] = spin_interval_this;
+			}
+		}
+		if(fly_spin < FLY_SPIN_COUNT)
+			spin_interval_this = 0;
+	}
+	if(RBUMPER->values[0] != 0 && !fire_last) {
+		fire_time = 0;
+		if(fly_spin >= FLY_SPIN_COUNT)
+			spin_interval_this = 0;
+	}
+	if(hook_time == 0 || fire_time == 0) {
+		if(FLY_SPIN_ENABLED && hook_time+fire_time < FLY_SPIN_WINDOW) {
+			fly_spin++;
+			if(fly_spin >= FLY_SPIN_COUNT) push_timer(0, SPIN_TICK, false);
+			if(fly_spin <= FLY_SPIN_COUNT) {
+				if(fly_spin == 2) {
+					fly_spin_interval = spin_interval_last;
+					fly_spin_intervals[0] = spin_interval_last;
+				}
+				fly_spin_interval = ((fly_spin-1)*fly_spin_interval+spin_interval_last)/fly_spin;
+				fly_spin_intervals[fly_spin-1] = spin_interval_last;
+			}
+			else {
+				int index = fly_spin%FLY_SPIN_COUNT;
+				fly_spin_interval = (FLY_SPIN_COUNT*fly_spin_interval-fly_spin_intervals[index]+spin_interval_last)/FLY_SPIN_COUNT;
+				fly_spin_intervals[index] = spin_interval_last;
+			}
+		}
+		else if(hook_time+fire_time > 2000) fly_spin = 0;
+	}
+	if(fly_spin >= FLY_SPIN_COUNT) idle_spin = 0;
+	if(idle_spin >= IDLE_SPIN_COUNT && abs(a-idle_spin_center) > IDLE_ANGLE_RANGE/2) idle_spin = 0;
+	bool hookline_off = false;
+	if(
+		(
+			idle_spin >= IDLE_SPIN_COUNT ||
+			fly_spin >= FLY_SPIN_COUNT
+		) && (
+			(fly_spin >= FLY_SPIN_COUNT)
+				? (spin_interval_this < fly_spin_interval)
+				: (idle_spin_interval > IDLE_SPIN_MIN_INTERVAL && spin_interval_this < idle_spin_interval)
+		) &&
+		hook_time > 1.5*(1000/50.0) && fire_time > 1.5*(1000/50.0)
+	) {
+		if(spin_interval_this > 1.5*(1000/50.0)) {
+			a += (fly_spin >= FLY_SPIN_COUNT)
+				?                                  (spin_interval_this/fly_spin_interval)*(2*M_PI)
+				: floor(idle_spin/IDLE_SPIN_COUNT)*(spin_interval_this/idle_spin_interval)*(2*M_PI)
+			;
+			con->inputs[CNTLR_RTHUMB].values[0] = cos(a);
+			con->inputs[CNTLR_RTHUMB].values[1] = sin(a);
+			hookline_off = true;
+		}
+		push_timer(0, SPIN_TICK, false);
+	}
+	AutoRelease(50, Extend(50, &kb->inputs[KB_EXTRA[extra_disable_hookline]])).set(0, (hookline_off) ? 1 : 0);
+	AutoRelease(50, Extend(50, &kb->inputs[KB_EXTRA[extra_enable_hookline]])).set(0, (hookline_off) ? 0 : 1);
+	/*}}}*/
 /*}}}*/
 
 /*{{{ left trigger*/
@@ -242,6 +357,7 @@ void user_process_frame() {
 	kb->inputs[KB_EXTRA[extra_spec]] = *HOME;
 	con->inputs[CNTLR_LBUMPER] = *LBUMPER;
 	con->inputs[CNTLR_RBUMPER] = *RBUMPER;
+	fire_last = RBUMPER->values[0] != 0;
 	con->inputs[CNTLR_Y] = *Y;
 	con->inputs[CNTLR_A] = *A;
 	con->inputs[CNTLR_X] = *X;
